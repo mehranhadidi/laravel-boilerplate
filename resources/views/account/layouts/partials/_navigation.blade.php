c5a67f2614e074dc3c721f189be860701e01e5c1
Mehran <mehran.hadidi@gmail.com>
2018-01-06 13:53:11 +0330

    account overview and profile update

---
+++ resources/views/account/layouts/partials/_navigation.blade.php
@@ -0,0 +1,4 @@
+<ul class="nav nav-pills nav-stacked nav-">
+    <li><a href="{{ route('account.index') }}">Account Overview</a></li>
+    <li><a href="{{ route('account.profile.index') }}">Profile</a></li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,15 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/dashboard', 'DashboardController@index')->name('home');
+/**
+ * Account
+ */
+Route::group(['prefix' => 'account', 'middleware' => 'auth', 'namespace' => 'Account', 'as' => 'account.'], function () {
+    Route::get('/', 'AccountController@index')->name('index');
+
+    /**
+     * Profile
+     */
+    Route::get('/profile', 'ProfileController@index')->name('profile.index');
+    Route::post('/profile', 'ProfileController@store')->name('profile.store');
+});
